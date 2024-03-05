c059405482a946c2855f03ed3da89b8eef46bc99
evergreen0621 <162002836+evergreen0621@users.noreply.github.com>
2024-03-06 01:25:08 +0900

    Create 1106.java

---
+++ 1106.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        int a = 0x80000000;
+        int b = 0x7fffffff;
+
+        System.out.printf("%d %d", a, b);
+    }
+}
