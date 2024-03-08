a516042f36f187f6b6d438b27b1121128ce6491f
evergreen0621 <162002836+evergreen0621@users.noreply.github.com>
2024-03-08 19:28:10 +0900

    Create 1157.java

---
+++ 1157.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        float a = sc.nextFloat();
+
+        if(a >= 50 && a <= 60){
+            System.out.println("win");
+        }
+        else{
+            System.out.println("lose");
+        }
+    }
+}
