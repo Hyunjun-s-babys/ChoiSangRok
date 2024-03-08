cb493b2cb61de9278c5548f184a63b8d0f3b8ab4
evergreen0621 <162002836+evergreen0621@users.noreply.github.com>
2024-03-08 20:42:16 +0900

    Create 1161.java

---
+++ 1161.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in, "UTF-8");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a % 2 == 0){
+            if(b % 2 == 0){
+                System.out.println("짝수+짝수=짝수");
+            }
+            else{
+                System.out.println("짝수+홀수=홀수");
+            }
+        }
+        else{
+            if(b % 2 == 0){
+                System.out.println("홀수+짝수=홀수");
+            }
+            else{
+                System.out.println("홀수+홀수=짝수");
+            }
+        }
+    }
+}
