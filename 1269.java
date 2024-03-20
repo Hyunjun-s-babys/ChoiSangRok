19dbcf6d3dbb064405a6fdd01825b36239ab34fb
Sangrok Choi <162002836+evergreen0621@users.noreply.github.com>
2024-03-20 19:04:38 +0900

    Create 1269.java

---
+++ 1269.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int n = sc.nextInt();
+
+        for(int i = 2; i <= n; i++){
+            a = a * b + c;
+        }
+
+        System.out.println(a);
+    }
+}
