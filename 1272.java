fc3acf4ef561ad9f5e9881cd04aab19ffe18a5bc
Sangrok Choi <162002836+evergreen0621@users.noreply.github.com>
2024-03-20 19:25:50 +0900

    Create 1272.java

---
+++ 1272.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int h = sc.nextInt();
+
+        int max = k >= h ? k : h;
+        int count = 1;
+        int seq;
+        int sum = 0;
+
+        for(int i = 1; i <= max; i++){
+            if(i % 2 == 0){
+                seq = count * 10;
+                count++;
+            }
+            else{
+                seq = count;
+            }
+            
+            if(i == k){
+                sum += seq;
+            }
+            if(i == h){
+                sum += seq;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
