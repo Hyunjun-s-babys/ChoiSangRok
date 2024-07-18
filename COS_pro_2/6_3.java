951ef11af9f4c3fd389d3419a3fa2540e3b49207
Sangrok Choi <162002836+evergreen0621@users.noreply.github.com>
2024-07-18 20:55:04 +0900

    Create 6_3.java

---
+++ COS_pro_2/6_3.java
@@ -0,0 +1,19 @@
+    public int[] solution(int[] people) {
+        
+        int[] answer = new int[4];
+				for(int i = 0; i < people.length; i++){
+					if(people[i] < 95){
+						answer[0]++;
+					}
+					else if(people[i] < 100){
+						answer[1]++;
+					}
+					else if(people[i] < 105){
+						answer[2]++;
+					}
+					else{
+						answer[3]++;
+					}
+				}
+        return answer;
+    }
